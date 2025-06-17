85c1965408ab311edbdb70697d18bef74c69e267
jjnr0203 <jonathan08192016@gmail.com>
2025-06-17 00:49:29 -0500

    fix update routes

--- src/app/pages/update/update.routes.ts
+++ src/app/pages/update/update.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from "@angular/router";
 import { AcomodationComponent } from "./activities/acomodation/acomodation.component";
 import { FoodComponent } from "./activities/food/food.component";
+import { AgencyComponent } from "./activities/agency/agency.component";
+import { AttractionComponent } from "./activities/attraction/attraction.component";
+import { CommunityComponent } from "./activities/community/community.component";
+import { GuidanceComponent } from "./activities/guidance/guidance.component";
+import { OrganizerComponent } from "./activities/organizer/organizer.component";
+import { TransportComponent } from "./activities/transport/transport.component";
 
 export default [
     {path:'acomodation', component: AcomodationComponent},
-    {path:'food', component: FoodComponent}
+    {path:'agency', component: AgencyComponent},
+    {path:'attraction', component: AttractionComponent},
+    {path:'community', component: CommunityComponent},
+    {path:'food', component: FoodComponent},
+    {path:'guidance', component: GuidanceComponent},
+    {path:'organizer', component: OrganizerComponent},
+    {path:'transport', component: TransportComponent},
 ] as Routes;
